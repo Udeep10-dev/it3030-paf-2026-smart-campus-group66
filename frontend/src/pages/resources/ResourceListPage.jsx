edf94c25f6ae448b765b0137af28e987c85a8dbc
vishanisomarathna <vishanisomarathna@gmail.com>
2026-04-14 23:07:53 +0530

    ui done

--- frontend/src/pages/resources/ResourceListPage.jsx
+++ frontend/src/pages/resources/ResourceListPage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { getResources } from "../../api/resources/resourceApi.js";
 import ResourceCard from "../../components/resources/ResourceCard";
 import ResourceFilter from "../../components/resources/ResourceFilter";
+import { Link } from "react-router-dom";
 
 const ResourceListPage = () => {
   const [resources, setResources] = useState([]);
@@ -13,14 +14,26 @@ const ResourceListPage = () => {
     setResources(res.data);
   };
 
-  useEffect(() => { fetchResources(); }, [filters]);
+  useEffect(() => {
+    fetchResources();
+  }, [filters]);
 
   return (
     <div className="min-h-screen p-6" style={{ background: "#FFF8F3" }}>
-      <h1 className="text-2xl font-medium mb-5" style={{ color: "#7C3B0A" }}>
-        Resources
-      </h1>
-      
+      <div className="flex justify-between items-center mb-5">
+        <h1 className="text-2xl font-medium" style={{ color: "#7C3B0A" }}>
+          Resources
+        </h1>
+
+        <Link to="/resources/new">
+          <button
+            className="text-white px-4 py-2 rounded"
+            style={{ background: "#D85A30" }}
+          >
+            + Add Resource
+          </button>
+        </Link>
+      </div>
       <ResourceFilter setFilters={setFilters} />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
         {resources.map((r) => (
