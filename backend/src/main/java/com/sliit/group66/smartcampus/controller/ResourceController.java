9d3c8ac6374d0110710504dbdc7f5f15d4ab6715
vishanisomarathna <vishanisomarathna@gmail.com>
2026-04-16 16:33:19 +0530

    add controller

---
+++ backend/src/main/java/com/sliit/group66/smartcampus/controller/ResourceController.java
@@ -0,0 +1,42 @@
+package com.example.resource.controller;
+
+import com.example.resource.dto.ResourceDTO;
+import com.example.resource.service.ResourceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/resources")
+@RequiredArgsConstructor
+@CrossOrigin
+public class ResourceController {
+
+    private final ResourceService service;
+
+    @PostMapping
+    public ResourceDTO create(@RequestBody ResourceDTO dto) {
+        return service.create(dto);
+    }
+
+    @GetMapping
+    public List<ResourceDTO> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResourceDTO getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResourceDTO update(@PathVariable Long id, @RequestBody ResourceDTO dto) {
+        return service.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
