4b5123ec4622657d48839157b320ed967bda0b2a
vishanisomarathna <vishanisomarathna@gmail.com>
2026-04-16 20:08:33 +0530

    update

--- frontend/src/services/resourceService.js
+++ frontend/src/services/resourceService.js
@@ -7,7 +7,7 @@ const resourceService = {
   getById:   (id)         => api.get(`${BASE}/${id}`),
   create:    (data)       => api.post(BASE, data),
   update:    (id, data)   => api.put(`${BASE}/${id}`, data),
-  remove:    (id)         => api.delete(`${BASE}/${id}`),
+  delete:    (id)         => api.delete(`${BASE}/${id}`),
 };
 
 export default resourceService;
