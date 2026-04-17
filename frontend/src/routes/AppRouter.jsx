65e5cf9e5e885a5c56b853cc52f8602b25367b81
rashmigimhani <rashmigimhani>
2026-04-17 22:04:55 +0530

    add booking routes and reorganize resource/admin route declarations

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,27 +1,39 @@
 import { Routes, Route } from "react-router-dom";
+
 import DashboardPage from "../pages/dashboard/DashboardPage";
 import LoginPage from "../pages/auth/LoginPage";
+
 import ResourceListPage from "../pages/resources/ResourceListPage";
-import BookingListPage from "../pages/booking/BookingListPage";
-import TicketListPage from "../pages/tickets/TicketListPage";
-import NotificationPage from "../pages/notifications/NotificationPage";
 import ResourceFormPage from "../pages/resources/ResourceFormPage";
 import ResourceAdminPage from "../pages/admin/resources/ResourceAdminPage";
+
+import BookingFormPage from "../pages/booking/BookingFormPage";
+import BookingListPage from "../pages/booking/BookingListPage";
+
+import TicketListPage from "../pages/tickets/TicketListPage";
+import NotificationPage from "../pages/notifications/NotificationPage";
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<DashboardPage />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/resources" element={<ResourceListPage />} />
-      <Route path="/bookings" element={<BookingListPage />} />
-      <Route path="/tickets" element={<TicketListPage />} />
-      <Route path="/notifications" element={<NotificationPage />} />
+
+      {/* Resources */}
       <Route path="/resources" element={<ResourceListPage />} />
       <Route path="/resources/new" element={<ResourceFormPage />} />
       <Route path="/admin/resources" element={<ResourceAdminPage />} />
+
+      {/* Bookings */}
+      <Route path="/bookings/new" element={<BookingFormPage />} />
+      <Route path="/admin/bookings" element={<BookingListPage />} />
+
+      {/* Other */}
+      <Route path="/tickets" element={<TicketListPage />} />
+      <Route path="/notifications" element={<NotificationPage />} />
     </Routes>
   );
 }
 
 export default AppRouter;
+``
