96419275bc162b217d174c9e3c2423f170fb897f
Tharu234 <piumali698@gmail.com>
2026-04-20 00:15:03 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,20 @@
- main
 import { Toaster } from "react-hot-toast";
-
 import { AuthProvider } from "./context/AuthContext";
 import Navbar from "./components/layout/Navbar";
- feature/member4-auth-notifications
 import AppRouter from "./routes/AppRouter";
 import "./styles/app.css";
 
 function App() {
   return (
- main
-    <>
+
+      <AuthProvider>
       <Toaster position="top-right" reverseOrder={false} />
       <AppRouter />
-    </>
+  
 
-    <AuthProvider>
       <Navbar />
       <AppRouter />
-    </AuthProvider>
- feature/member4-auth-notifications
+      </AuthProvider>
   );
 }
 
